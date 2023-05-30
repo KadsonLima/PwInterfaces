d808bc9a7de2b3119da30e57c9d2d9caea0418fa
Kadson Lima <kadson-kaio@hotmail.com>
2023-05-30 14:28:03 -0300

    styles: chat window

--- src/components/ChatBox/ChatWindow.tsx
+++ src/components/ChatBox/ChatWindow.tsx
@@ -1,4 +1,4 @@
-import { Box, Text, Button } from "@chakra-ui/react";
+import { Box, Text, Button, Flex } from "@chakra-ui/react";
 import { stylesColor } from "@styles/colors";
 import { useState } from "react";
 
@@ -30,13 +30,15 @@ const ChatWindow: React.FC<ChatWindowProps> = ({ onClose }) => {
       w="100%"
       maxWidth="400px"
       height="500px"
-      bg={stylesColor.backgroundColor}
+      bg="#201e1e"
       boxShadow="md"
       zIndex={3}
       borderRadius="md"
+      borderWidth={2}
+      borderColor="#c90505"
       p="4"
-    >
-      <Text fontSize="lg" fontWeight="bold" mb="4">
+    ><Flex w="100%" h="100%" position="relative" flexDirection="column" gap={4}>
+      <Text fontSize="lg" fontWeight="bold" mb="4" align="center" color="#c90505">
         Bem-vindo ao Chat
       </Text>
 
@@ -56,9 +58,10 @@ const ChatWindow: React.FC<ChatWindowProps> = ({ onClose }) => {
         </>
       )}
 
-      <Box textAlign="center">
+      <Box textAlign="center" position="absolute" bottom={0}>
         <Button onClick={onClose}>Fechar Chat</Button>
       </Box>
+      </Flex>
     </Box>
   );
 };
