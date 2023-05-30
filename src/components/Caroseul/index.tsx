7039e8db5c63eac37930b6e94f445ba51e8b990d
Kadson Lima <kadson-kaio@hotmail.com>
2023-05-30 18:56:14 -0300

    feat: images caroseul

--- src/components/Caroseul/index.tsx
+++ src/components/Caroseul/index.tsx
@@ -22,52 +22,56 @@ const CarouselComponent = () => {
       <Grid templateColumns="repeat(4, 1fr)" gap={4} p={4}>
         <VideoComponent link="EfWf8gBxuI4"/>
         <Image
-          src="https://img.freepik.com/vetores-gratis/vetor-de-design-de-gradiente-colorido-de-passaro_343694-2506.jpg"
+          src="https://i.imgur.com/XMMddOb.gif"
           alt="Image 1"
           _hover={{ cursor: "pointer", opacity: 0.8 }}
           onClick={() =>
             handleItemClick(
-              "https://img.freepik.com/vetores-gratis/vetor-de-design-de-gradiente-colorido-de-passaro_343694-2506.jpg"
+              "https://i.imgur.com/XMMddOb.gif"
             )
           }
           cursor="pointer"
         />
         <Image
           _hover={{ cursor: "pointer", opacity: 0.8 }}
-          src="https://png.pngtree.com/element_our/png/20180912/coffee-time-png_91570.jpg"
+          src="https://i.imgur.com/UqidhpG.gif"
           alt="Image 2"
           onClick={() =>
             handleItemClick(
-              "https://png.pngtree.com/element_our/png/20180912/coffee-time-png_91570.jpg"
+              "https://i.imgur.com/UqidhpG.gif"
             )
           }
         />
         <VideoComponent link="zD-Fy8e7hF0"/>
         <Image
-          src="https://img.freepik.com/vetores-gratis/vetor-de-design-de-gradiente-colorido-de-passaro_343694-2506.jpg"
+          src="https://i.imgur.com/r6WKUt6.png"
           alt="Image 1"
           _hover={{ cursor: "pointer", opacity: 0.8 }}
           onClick={() =>
             handleItemClick(
-              "https://img.freepik.com/vetores-gratis/vetor-de-design-de-gradiente-colorido-de-passaro_343694-2506.jpg"
+              "https://i.imgur.com/r6WKUt6.png"
             )
           }
         />
         <Image
-          src="https://png.pngtree.com/element_our/png/20180912/coffee-time-png_91570.jpg"
+          src="https://i.imgur.com/LhAq27E.gif"
           alt="Image 2"
           _hover={{ cursor: "pointer", opacity: 0.8 }}
           onClick={() =>
             handleItemClick(
-              "https://images.unsplash.com/photo-1614851099175-e5b30eb6f696?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGJhbm5lcnxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
+              "https://i.imgur.com/LhAq27E.gif"
             )
           }
         />
+        <VideoComponent link="7WI-I8wgy9g"/>
+        <VideoComponent link="yQpAL2R6_Gw"/>
+        <VideoComponent link="rmLHoPQVjQc"/>
+        <VideoComponent link="YJhqjc4_sn0"/>
       </Grid>
 
       <Modal isOpen={!!selectedItem} onClose={handleCloseModal} size="xl">
       <ModalOverlay />
-      <ModalContent display={"flex"} alignItems="center">
+      <ModalContent display={"flex"} alignItems="center" bg={stylesColor.backgroundColor}>
         <ModalCloseButton bg="red.700" />
         <ModalBody p={4} bg={stylesColor.backgroundColor}>
           {selectedItem && <Image src={selectedItem} alt="Selected Item" width="100%" />}
