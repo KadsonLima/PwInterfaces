b627aca0bc21901252bf9b37b09c5dffaa09eb20
Kadson Lima <kadson-kaio@hotmail.com>
2023-07-08 14:22:36 -0300

    feat: system cupom change

--- src/components/FormButton/FormScreen/index.tsx
+++ src/components/FormButton/FormScreen/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import {
-  Container,
+  Text,
   Modal,
   ModalContent,
   ModalHeader,
@@ -9,89 +9,143 @@ import {
   FormLabel,
   Input,
   Textarea,
-  Checkbox,
+  Flex,
   Button,
   CloseButton,
   ChakraProvider,
   extendTheme,
+  Spinner,
+  useToast,
 } from "@chakra-ui/react";
-import axios from 'axios';
 
-// Define o tema personalizado
 const theme = extendTheme({
   colors: {
     dark: {
-      900: "##181616", // Cor de fundo escura
+      900: "#181616", // Cor de fundo escura
       800: "#ffffff", // Cor do texto em branco
       red: "#bd0505", // Cor vermelha para bordas
     },
   },
 });
 
-export default function FormModal({showModal,setShowModal}:{showModal:boolean, setShowModal:Function}) {
+export default function FormModal({
+  showModal,
+  setShowModal,
+}: {
+  showModal: boolean;
+  setShowModal: Function;
+}) {
+  const [valores, setValores] = useState([50, 150, 200]);
+  useEffect(() => { console.log(valores)}, [showModal, valores]);
 
-  useEffect(() => {
-
-  }, [showModal])
-
+  const [loading, setLoading] = useState(false);
   const [isOpen, setIsOpen] = useState(showModal);
   const [formValues, setFormValues] = useState({
     nome: "",
     contato: "",
-    service: {
-      converter: false,
-      criacao: false,
-    },
+    service: "",
     server: "",
     description: "",
-    cupom:"",
+    cupom: "",
   });
+  const toast = useToast();
 
   const handleInputChange = (e:any) => {
     const { name, value } = e.target;
-    console.log(formValues)
     setFormValues((prevValues) => ({
       ...prevValues,
       [name]: value,
     }));
+    if (name === "cupom") {
+      if(value.length < 6) return;
+
+      const cupomEncontrado = CUPOMS.find((cupom) => cupom.nome === value.toUpperCase());
+
+      if (cupomEncontrado) {
+        if(!cupomEncontrado.offer){
+          toast({
+            title: "Cupom Não Disponivel.",
+            status: "error",
+          });
+          return;
+        }
+
+        setValores(valores.map((valor)=>{
+          return valor - (valor * cupomEncontrado.valor);
+        }))
+        
+        toast({
+          title: "Cupom Encontrado.",
+          status: "success",
+        });
+      } else {
+        toast({
+          title: "Cupom Não Encontrado.",
+          status: "error",
+        });
+      }
+    }
   };
 
-  const handleCheckboxChange = (e:any) => {
-    const { name, checked } = e.target;
-    setFormValues((prevValues) => ({
-      ...prevValues,
-      service: {
-        ...prevValues.service,
-        [name]: checked,
-      },
-    }));
-  };
-
-  
   const handleSubmit = async (e:any) => {
     e.preventDefault();
-    
-    console.log("cupom a enviar", formValues)
-    const response = await fetch('https://sheetdb.io/api/v1/tpxvb7uy2ba1q', {
-      method: 'POST',
+    setLoading(true);
+    const response = await fetch("https://sheetdb.io/api/v1/tpxvb7uy2ba1q", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ data: formValues }),
     });
-    
-    const data = await response.json();
-    console.log(data);
 
-    setIsOpen(false);
-    setShowModal(false);
+    if (response.ok) {
+      toast({
+        title: "Pedido Criado.",
+        description: "Seu Pedido foi enviado com Sucesso.",
+        status: "success",
+        duration: 9000,
+        isClosable: true,
+      });
+      setLoading(false);
+      setFormValues({
+        nome: "",
+        contato: "",
+        service: [],
+        server: "",
+        description: "",
+        cupom: "",
+      });
+    } else {
+      setLoading(false);
+      toast({
+        title: "Error.",
+        description:
+          "Não foi possivel fazer o envio do seu pedido, por favor entre em contato!.",
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+      });
+    }
   };
 
+  const choiceService = (name:any) =>{
+    setFormValues((prevValues) => ({
+      ...prevValues,
+      service: name,
+    }));
+
+  }
+
   return (
-    <ChakraProvider theme={theme}> {/* Aplica o tema personalizado */}
+    <ChakraProvider theme={theme}>
       <Modal isOpen={isOpen} onClose={() => setShowModal(false)}>
-        <ModalContent bg="#312f2f" color="dark.800" style={{border:" solid 3px #bd0505"}} padding={4}> {/* Define a cor de fundo e a cor do texto */}
-          <ModalHeader>Pedido</ModalHeader>
+        <ModalContent
+          bg="#312f2f"
+          color="dark.800"
+          style={{ border: " solid 3px #bd0505" }}
+          padding={4}
+        >
+          <ModalHeader textAlign="center" fontSize={"2xl"} color={"gold"}>Fazer Pedido</ModalHeader>
           <ModalBody>
             <form onSubmit={handleSubmit}>
               <FormControl>
@@ -102,9 +156,10 @@ export default function FormModal({showModal,setShowModal}:{showModal:boolean, s
                   value={formValues.nome}
                   onChange={handleInputChange}
                   required
-                  bg="#141313" // Define a cor de fundo
-                  color="dark.800" // Define a cor do texto
+                  bg="#141313"
+                  color="dark.800"
                   border="3px solid #bd0505"
+                  
                 />
               </FormControl>
               <FormControl marginBottom={5}>
@@ -120,28 +175,61 @@ export default function FormModal({showModal,setShowModal}:{showModal:boolean, s
                   border="3px solid #bd0505"
                 />
               </FormControl>
-            
+
               <FormControl marginBottom={5}>
                 <FormLabel>Serviço:</FormLabel>
-                <Checkbox
-                  name="converter"
-                  checked={formValues.service.converter}
-                  onChange={handleCheckboxChange}
-                  marginRight={10}
-                  colorScheme="red" // Define a cor do checkbox quando selecionado
-                >
-                  Converter
-                </Checkbox>
-                <Checkbox
-                  name="criacao"
-                  checked={formValues.service.criacao}
-                  onChange={handleCheckboxChange}
-                  colorScheme="red"
-                >
-                  Criação
-                </Checkbox>
+                <Flex justifyContent="space-evenly" flexWrap="wrap" rowGap={2}>
+                  <Flex
+                    flexDirection="column"
+                    bg="#bd0505"
+                    width={155}
+                    height={50}
+                    justifyContent="center"
+                    align="center"
+                    borderRadius={4}
+                    fontWeight={700}
+                    border={formValues.service == `converter ${valores[0]}` ? "2px solid white" : "none"}
+                    _hover={{border:"2px solid white", cursor:"pointer"}}
+                    onClick={()=>choiceService(`converter ${valores[0]}`)}
+                  >
+                    <span>Converter</span>
+                    <p>{valores[0]} R$</p>
+                  </Flex>
+                  <Flex
+                    flexDirection="column"
+                    bg="#bd0505"
+                    width={155}
+                    height={50}
+                    justifyContent="center"
+                    align="center"
+                    borderRadius={4}
+                    fontWeight={700}
+                    border={formValues.service == `converter ${valores[1]}` ? "2px solid white" : "none"}
+                    _hover={{border:"2px solid white", cursor:"pointer"}}
+                    onClick={()=>choiceService(`converter ${valores[1]}`)}
+                  >
+                    <span>Ui Basic</span>
+                    <p>{valores[1]} R$</p>
+                  </Flex>
+                  <Flex
+                    flexDirection="column"
+                    bg="#bda105"
+                    width={155}
+                    height={50}
+                    justifyContent="center"
+                    align="center"
+                    borderRadius={4}
+                    fontWeight={700}
+                    border={formValues.service == `converter ${valores[2]}` ? "2px solid white" : "none"}
+                    _hover={{border:"2px solid white", cursor:"pointer"}}
+                    onClick={()=>choiceService(`converter ${valores[2]}`)}
+                  >
+                    <span>Ui Premium</span>
+                    <p>{valores[2]} R$</p>
+                  </Flex>
+                </Flex>
               </FormControl>
-              
+
               <FormControl marginBottom={5}>
                 <FormLabel>Servidor:</FormLabel>
                 <Input
@@ -166,7 +254,12 @@ export default function FormModal({showModal,setShowModal}:{showModal:boolean, s
                   border="3px solid #bd0505"
                 />
               </FormControl>
-              <FormControl display="flex" justifyContent="right" marginBottom={5} alignItems="center">
+              <FormControl
+                display="flex"
+                justifyContent="right"
+                marginBottom={5}
+                alignItems="center"
+              >
                 <FormLabel>Cupom/Referência:</FormLabel>
                 <Input
                   width="150px"
@@ -174,15 +267,17 @@ export default function FormModal({showModal,setShowModal}:{showModal:boolean, s
                   name="cupom"
                   value={formValues.cupom}
                   onChange={handleInputChange}
-                  required
+                  required={false}
                   bg="#141313"
                   color="dark.800"
                   border="3px solid #bd0505"
                 />
               </FormControl>
-              <Button colorScheme="green" type="submit"> {/* Define a cor do botão */}
-                Enviar
-              </Button>
+              <FormControl display="flex" justifyContent="center">
+                <Button colorScheme="green" type="submit" w={120}>
+                  {loading ? <Spinner /> : "Enviar"}
+                </Button>
+              </FormControl>
               <CloseButton
                 size="sm"
                 onClick={() => setShowModal(false)}
@@ -197,3 +292,10 @@ export default function FormModal({showModal,setShowModal}:{showModal:boolean, s
     </ChakraProvider>
   );
 }
+
+const CUPOMS = [
+  { nome: "CUPOM10", valor: 0.1, offer: true },
+  { nome: "OFERTA20", valor: 0.2, offer: true },
+  { nome: "PROMO30", valor: 0.3, offer: true },
+  { nome: "KASH40", valor: 0.4, offer: true },
+];
