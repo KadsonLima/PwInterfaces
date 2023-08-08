a5c087aa67796db171ee32bcdd0d675ab59e0b84
Kadson Lima <kadsonkaio@gmail.com>
2023-08-07 21:15:47 -0300

    feat: adsense config

--- src/components/AdSense/index.tsx
+++ src/components/AdSense/index.tsx
@@ -1,11 +1,11 @@
 import Adsense from "@eisberg-labs/next-google-adsense";
-import Adunit from "@eisberg-labs/next-google-adsense/dist/adunit";
+import {AdUnit} from "@eisberg-labs/next-google-adsense";
 
 export default function AdSense(){
     return (
         <>
           <Adsense client_id="ca-pub-7681391913996548"/>
-          <Adunit className="adsbygoogle block"
+          <AdUnit className="adsbygoogle block"
                 data-ad-client="ca-pub-7681391913996548"
                 data-ad-slot="..."
                 data-ad-format="auto"
