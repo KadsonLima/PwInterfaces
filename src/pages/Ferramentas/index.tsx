79d42dff4dabb0999da65a8f3b3a4cb3a5bbc057
Kadson Lima <kadson-kaio@hotmail.com>
2023-10-08 12:17:22 -0300

    fix

--- src/pages/Ferramentas/index.tsx
+++ src/pages/Ferramentas/index.tsx
@@ -1,13 +1,10 @@
 import { Flex, Box, Heading, Text, Button, AspectRatio } from "@chakra-ui/react";
-import DynamicBanner from "../../components/Banner";
 import { PageContainer } from "../../components/pageContainer";
-import AdSense from "../../components/AdSense";
 
 
 const Tools = () => {
   return (
     <PageContainer>
-      <AdSense/>
       <Box w="100%" marginTop={3} marginBottom={0} bg={"black"}>
       <iframe
         src="/tools/index.html"
